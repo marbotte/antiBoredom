3fa3abb53a7166fd007763113cf7ddc0c4705964
marbotte <bottinmarius@gmail.com>
2021-12-12 16:06:12 -0500

    Suppressed renderMainPage

--- endpoint_def.py
+++ endpoint_def.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 from flask_restful import Resource, Api
 from main import exp_act_joke
 from main import export_log
-from main import renderMainPage
 import psycopg2
 import os
 
