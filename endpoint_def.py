643e165beb8c5fcd4ccf7e5bdd19602980942933
marbotte <bottinmarius@gmail.com>
2021-12-12 08:57:37 -0500

    Update core function for ActJoke endpoint

--- endpoint_def.py
+++ endpoint_def.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from flask_restful import Resource, Api, reqparse
-from my_functions import act_joke
+from flask_restful import Resource, Api
+from my_functions import exp_act_joke
 
 class ActJoke(Resource):
     def get(self, type_act):
-        return act_joke(type_act)
+        return exp_act_joke(type_act)
     
--- my_functions.py
+++ my_functions.py
@@ -3,12 +3,13 @@ import requests
 import spacy
 import random
 import re
+import json
 
 
 
 def get_activity(type_act: str):
     if type_act not in ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]:
-        return "I am a computer... As such I am very dumb, and I did not reocognise your activity type. It should be one of 'education', 'recreational', 'social', 'diy', 'charity', 'cooking', 'relaxation', 'music', 'busywork'"
+        return {"unrecognized": True, "activity": "I am a computer... As such I am very dumb, and I did not reocognise your activity type. It should be one of 'education', 'recreational', 'social', 'diy', 'charity', 'cooking', 'relaxation', 'music', 'busywork'", "key": None}
     api = f"https://www.boredapi.com/api/activity?type={type_act}"
     response = requests.get(api)
     content = response.json()
@@ -22,16 +23,16 @@ def extract_noun(text: str):
 def extract_words(text: str):
     pattern = '[A-Za-z]+'
     words = re.findall(pattern, text)
-    del_items = ["A", "a", "s", "An", "an", "The", "the", "One", "one", "is", "This", "this", "That", "that", "get", "on", "up", "down", "in", "your", "to", "with", "make", "are", "be", "go", "some", "have", "Have"]
+    del_items = ["A", "a", "s", "An", "an", "The", "the", "One", "one", "is", "This", "this", "That", "that", "get", "on", "up", "down", "in", "your", "to", "with", "make", "are", "be", "go", "some", "have", "Have", "and", "it", "out", "at", "I", "am", "very", "not"]
     return [x for x in words if x not in del_items]
 
 def get_joke(word_contains: str):
     api = f"https://v2.jokeapi.dev/joke/Any?contains={word_contains}"
     response = requests.get(api)
     content = response.json()
-    joke = "This is a very boring activity"
+    joke = None
     if content["error"]:
-        return {"error": content["error"], "joke": joke, "id": type(None)}
+        return {"error": content["error"], "joke": joke, "id": None}
     else:
         if content["type"] == 'single':
             joke = content["joke"]
@@ -43,12 +44,8 @@ def get_joke(word_contains: str):
 
 def act_joke(type_act: str):
     act = get_activity(type_act)
-    print(act)
     extracted = extract_words(act["activity"])
-    print(extracted)
-    if len(extracted) == 0:
-        res_joke = {"error": True, "joke": "This is a very boring activity", "id": type(None)}
-    elif len(extracted) == 1:
+    if len(extracted) < 2 or act["unrecognized"]:
         word = extracted[0]
         res_joke = get_joke(word)
     else:
@@ -60,5 +57,13 @@ def act_joke(type_act: str):
             word = extracted[1]
             res_joke = get_joke(word)
     print(word)
+    extensive_res = {**act, **res_joke, **{"word": word}}
+    return extensive_res
 #    return extracted
-    return {"activity": act["activity"], "joke": res_joke["joke"]}
+#    return {"activity": act["activity"], "joke": res_joke["joke"]}
+
+def exp_act_joke(type_act: str):
+    extensive_res = act_joke(type_act)
+    return json.dumps({"activity": extensive_res["activity"], "joke": extensive_res["joke"]})
+
+
